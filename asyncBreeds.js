d4fb98bdbd85794cd6751a96d29a4fbdc4d450ca
Dexter Chan <dexterchann@gmail.com>
2020-03-10 22:26:03 +0000

    Added mocha and chai tests

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -6,14 +6,13 @@ const cb = (data) => {
 };
 
 const breedDetailsFromFile = function(breed, cb) {
-  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
     if (!error) {
       cb(data);
+    } else {
+      cb(undefined);
     }
   });
 };
 
-// we try to get the return value
-breedDetailsFromFile('Bombay', cb);
+module.exports = breedDetailsFromFile;
